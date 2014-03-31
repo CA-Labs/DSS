88da7be252a02c4ffbae8e0a8764142894799e44
Jacek Dominiak <jacek@effortable.com>
2014-03-31 13:13:11 +0200

    migrated new design

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,35 @@ module.exports = function (grunt) {
 
     // Project configuration
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            my_target: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: 'app/app.min.map',
+                    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        ' - author: <%= pkg.author %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+                },
+                files: {
+                    'app/app.min.js': [
+                        'app/app.js',
+
+                        'app/common/filters.js',
+                        'app/common/orientdbFactory.js',
+
+                        'requirements/requiremetnsController.js',
+                        'requirements/requiremetnsInterfacesDirective.js'
+                    ]
+                }
+            }
+        }
 
     });
 
     // Load packages
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Tasks
-    grunt.registerTask('default', []);
+    grunt.registerTask('default', ['uglify']);
 };
