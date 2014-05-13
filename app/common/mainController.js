5ceb9a288a41c4554643781adcf0fd61c6ccfcd9
jacek <jacek@effortable.com>
2014-05-13 17:00:41 +0200

    started saveSession export

--- app/common/mainController.js
+++ app/common/mainController.js
@@ -5,4 +5,14 @@ dssApp.controller('mainController', function ($scope, $rootScope, orientdbFactor
         $localStorage.risksSelected = $rootScope.risksSelected = [];
         $localStorage.requirementsSelected = $rootScope.requirementsSelected = [];
     };
+
+    $scope.saveSession = function (event) {
+        var element = angular.element(event.target);
+
+        element.attr({
+            href: 'data:application/json;charset=utf-8,' + encodeURI($localStorage.toString()),
+            target: '_blank',
+            download: 'filename.json'
+        });
+    };
 });
