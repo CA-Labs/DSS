cde2048a6bb60eb2620c2690c3cbb99d7e09710f
jacek <jacek@effortable.com>
2014-05-13 16:07:15 +0200

    added localstorage to assets

--- app/assets/assetsController.js
+++ app/assets/assetsController.js
@@ -1,13 +1,17 @@
-dssApp.controller('assetsController', function($scope, orientdbFactory) {
+dssApp.controller('assetsController', function($scope, orientdbFactory, $localStorage) {
    // extend base controller
     //$injector.invoke(baseController, this, {$scope: $scope});
 
     $scope.asset = "";
-    $scope.assetsSelected = [];
+    $scope.assetsSelected = $localStorage.assetsSelected || [];
     $scope.description = "";
     $scope.updateDescription = function (assetSelected) {
         $scope.asset.description = assetSelected.description;
     };
+
+    $scope.$watch("assetsSelected", function (value) {
+        $localStorage.assetsSelected = value;
+    });
 
     // fetch data
     orientdbFactory.getMatching('Assets', '', function (data) {
