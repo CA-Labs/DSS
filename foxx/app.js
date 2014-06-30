99d1e097ff8b95197d7484850c581fe3a9a1b5ae
Jordi Aranda <jordi.aranda@bsc.es>
2014-06-30 12:38:53 +0200

    Basic endpoints with comments to generate swagger doc

--- foxx/app.js
+++ foxx/app.js
@@ -1,12 +1,54 @@
 (function() {
-    "use strict";
-
-    var Foxx = require("org/arangodb/foxx"),
-        controller = new Foxx.Controller(applicationContext)
-
-    controller.get("/hello/:name", function(req, res) {
-        res.set("Content-Type", "text/plain");
-        res.body = "Hello " + req.params("name");
-    });
+  
+  "use strict";
+  
+  var Foxx = require("org/arangodb/foxx"),
+  controller = new Foxx.Controller(applicationContext)
+  
+  /************************************************************************
+  *********************** ASSETS-RELATED ENDPOINTS ************************
+  ************************************************************************/
+  
+  /** Retrieves all assets by asset type (bsoia/toia/)
+   * 
+   */      
+  controller.get("/assets/:type", function(req, res) {
+    var query = "for node in dss_nodes filter node.type == @type return node";
+    var stmt = db._createStatement({"query": query});
+    stmt.bind("type", req.params("type").toString);
+    var result = stmt.execute();
+    res.json(result);
+  }).pathParam("type", {
+    description: "The type of the asset (bsoia/toia)",
+    type: "string"
+  });
+  
+  /************************************************************************
+  ************************ RISKS-RELATED ENDPOINTS ************************
+  ************************************************************************/
+  
+  /** Retrieves all risks
+   * 
+   */
+  controller.get("/risks", function(req, res){
+    var query = "for node in dss_nodes filter node.type == 'risk' return node";
+    var stmt = db._createStatement({"query": query});
+    var result = stmt.execute();
+    res.json(result);
+  });
+  
+  /************************************************************************
+  ********************** TREATMENTS-RELATED ENDPOINTS *********************
+  ************************************************************************/
+  
+  /** Retrieves all treatments
+   * 
+   */
+  controller.get("/treatments", function(req, res){
+    var query = "for node in dss_nodes filter node.type == 'treatment' return node";
+    var stmt = db._createStatement({"query": query});
+    var result = stmt.execute();
+    res.json(result);
+  });
 
 }());
