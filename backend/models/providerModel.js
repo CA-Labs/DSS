1c7f1df862b04eaab28c4cccbba2281cdae26d0d
jacek <jacek@effortable.com>
2014-07-22 15:54:11 +0200

    added providerModel

---
+++ backend/models/providerModel.js
@@ -0,0 +1,26 @@
+/**
+ * Provider Model
+ *
+ * @author: Jacek Dominiak
+ * @copyright: Jacek Dominiak
+ * @created: 22/07/14
+ */
+
+var FoxxModel = require('org/arangodb/foxx').Model,
+    joi = require('joi'),
+    ProviderModel = FoxxModel.extend({
+        schema: {
+            name: joi.string().required(),
+            type: joi.string().required().default('provider'),
+            year_founding: joi.number().min(1800).max(Date.now.getFullYear()).optional(),
+            website: joi.string().optional().regex("(http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?"),
+            logo_url: joi.string().optional().regex("(http|ftp|https)://[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:/~+#-]*[\w@?^=%&amp;/~+#-])?"),
+            description: joi.string().required(),
+            number_of_employees: joi.number().optional(),
+            headquarters: joi.string().optional(),
+            headquarters_country: joi.string().optional(),
+            headquarters_continent: joi.string().optional()
+        }
+    });
+
+exports.model = ProviderModel;
