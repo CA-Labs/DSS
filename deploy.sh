071bd102ca11f3befda5fa1b4be54a891ebbffa8
jacek <jacek@effortable.com>
2014-07-22 15:06:26 +0200

    corrections to manifest and deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-foxx-manager purge dss && foxx-manager fetch directory foxx && foxx-manager mount app:dss:0.0.1 /dss && foxx-manager setup /dss
+foxx-manager purge dss && foxx-manager fetch directory . && foxx-manager mount app:dss:0.0.1 /dss && foxx-manager setup /dss
