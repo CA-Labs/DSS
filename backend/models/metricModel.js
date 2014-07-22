28c0fdb7c233c0e1dfa8113ba7ced3ec6e5abfc3
jacek <jacek@effortable.com>
2014-07-22 15:43:09 +0200

    added metricModel

---
+++ backend/models/metricModel.js
@@ -0,0 +1,18 @@
+/**
+ * Metric Model
+ * @author: Jacek Dominiak
+ * @copyright: Jacek Dominiak
+ * @created: 22/07/14
+ */
+
+var FoxxModel = require('org/arangodb/foxx').Model,
+    joi = require('joi'),
+    MetricModel = FoxxModel.extend({
+        schema: {
+            name: joi.string().required(),
+            type: joi.string().required().default('metric'),
+            options: joi.object()
+        }
+    });
+
+exports.model = MetricModel;
