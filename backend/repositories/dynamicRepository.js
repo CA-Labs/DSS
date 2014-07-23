753f86cbeeedbf5db81a6a38fd45eab24ee1794c
jacek <jacek@effortable.com>
2014-07-23 13:18:26 +0200

    added dynamic repository

--- backend/repositories/dynamicRepository.js
+++ backend/repositories/dynamicRepository.js
@@ -7,31 +7,6 @@
  */
 
 var FoxxRepository = require('org/arangodb/foxx').Repository,
-    DynamicRepository = FoxxRepository.extend({
-        getAll: function (modelName) {
-            var model = require('../models/' + modelName + 'Model').model;
-            return model.all().toArray();
-        },
-        getById: function (modelName, key) {
-            var model = require('../models/' + modelName + 'Model').model;
-            return model.byId(key).toArray();
-        },
-        getByAttributes: function (modelName, attributeValues) {
-            var model = require('../models/' + modelName + 'Model').model;
-            return model.byExample(attributeValues).toArray();
-        },
-        create: function (modelName, data) {
-            var model = require('../models/' + modelName + 'Model').model;
-            return model.save(data);
-        },
-        update: function (modelName, key, data) {
-            var model = require('../models/' + modelName + 'Model').model;
-            return model.replaceById(key, data);
-        },
-        remove: function (modelName, key) {
-            var model = require('../models/' + modelName + 'Model').model;
-            return model.remove(key);
-        }
-    });
+    DynamicRepository = FoxxRepository.extend({});
 
 exports.repository = DynamicRepository;
