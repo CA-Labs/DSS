917c59bcc522cc755fa909770c63d6221186cdd5
jacek <jacek@effortable.com>
2014-07-24 08:50:37 +0200

    crud controller almost working

--- backend/controllers/crudController.js
+++ backend/controllers/crudController.js
@@ -8,7 +8,10 @@
         nodesCollection = db._collection('dss_nodes'),
         edgesCollection = db._collection('dss_edges'),
         Repository = require('backend/repositories/dynamicRepository').repository,
-
+        MetricModel = require('backend/models/metricModel').model,
+        CharacteristicModel = require('backend/models/characteristicModel').model,
+        ProviderModel = require('backend/models/providerModel').model,
+        ServiceModel = require('backend/models/serviceModel').model,
         console = require('console'),
         joi = require('joi'),
         _ = require('underscore');
@@ -20,6 +23,35 @@
     //
 
     var nodesRepository = new Repository(nodesCollection, { });
+
+    var createModel = function (dataArray) {
+        var dataModel = [];
+        _.each(dataArray, function (data) {
+            console.log(_.isUndefined(data.type));
+            if (!_.isUndefined(data.type)) {
+                var model;
+                switch (data.type) {
+                    case 'metric':
+                        model = new MetricModel(data);
+                        break;
+                    case 'characteristic':
+                        model = new CharacteristicModel(data);
+                        break;
+                    case 'provider':
+                        model = new ProviderModel(data);
+                        break;
+                    case 'service':
+                        model = new ServiceModel(data);
+                        break;
+                    default:
+                        return false;
+                }
+                console.log(JSON.stringify(model));
+                if (model.isValid) dataModel.push(model);
+            }
+        });
+        return dataModel;
+    };
 
 
     //
@@ -53,7 +85,17 @@
      */
     controller.put('/node', function (req, res) {
         // for now no validation
-        res.json(nodesRepository.save(req.body()));
+        var responseData = [];
+        var data = createModel(req.body());
+        if (data) {
+            _.each(data, function (objectToSave) {
+                console.log('ffffff: ' + JSON.stringify(objectToSave.attributes));
+                responseData.push(nodesRepository.save("{" + objectToSave.attributes +  "}"));
+            });
+            res.json(responseData);
+        } else {
+            res.json({success: false});
+        }
     });
 
     /** Modifies the dss_node type of a service of a metric
@@ -61,7 +103,12 @@
      */
     controller.post('/node/:id', function (req, res) {
         // for now no validation
-        res.json(nodesRepository.replaceById(req.params('id')), req.body());
+        var data = createModel(req.body());
+        if (data) {
+            res.json(nodesRepository.replaceById(req.params('id')), data.attributes);
+        } else {
+            res.json({ success: false });
+        }
     }).errorResponse(arango.ArangoError, 404, "The document could not be found")
         .pathParam('id', {
             description: 'Id of the dss_nodes to be modified',
@@ -73,7 +120,7 @@
      */
     controller.delete('/node/:id', function (req, res) {
         // for now no validation
-        nodesRepository.remove(req.params('id'));
+        nodesRepository.removeById(req.params('id'));
         res.json({ status: true });
     }).errorResponse(arango.ArangoError, 404, "The document could not be found")
         .pathParam('id', {
