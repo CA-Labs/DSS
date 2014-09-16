11a79bc58965b7582347118228c1f682579d837e
jacek <jacek@effortable.com>
2014-09-16 15:24:41 +0200

    added cartesian function

--- frontend/services/cloud-service.js
+++ frontend/services/cloud-service.js
@@ -36,6 +36,26 @@ dssApp.service('CloudService', ['AssetsService', 'RisksService', 'TreatmentsServ
         return loadingProposals;
     };
 
+    /**
+     * returns array of all possible permutations of the arguments
+     * @returns {Array}
+     */
+    this.cartesian = function () {
+        var r = [], arg = arguments, max = arg.length-1;
+        function helper(arr, i) {
+            for (var j=0, l=arg[i].length; j<l; j++) {
+                var a = arr.slice(0); // clone arr
+                a.push(arg[i][j]);
+                if (i==max) {
+                    r.push(a);
+                } else
+                    helper(a, i+1);
+            }
+        }
+        helper([], 0);
+        return r;
+    };
+
     this.setTAProposals = function(ta, data){
         switch(ta.cloudType){
             case 'IaaS':
